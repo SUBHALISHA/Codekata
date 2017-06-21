7b3c91e589eb083b94a2a092b1b676339a12bee1
SUBHASREE K <subhasree260497@gmail.com>
2017-06-21 17:12:13 +0530

    Create even_range.py

---
+++ even_range.py
@@ -0,0 +1,19 @@
+i=0
+st=input("Enter starting limit:")
+dt=input("Enter ending limit:")
+if(st%2==0):
+  i=1
+  j=st
+else:
+  i=-1
+  j=st+1
+if(i==1):
+  print("The even numbers are:")
+  while j<=dt:
+    print(j)
+    j=j+2
+else:
+    print("The even numbers are:")
+    while j<=dt:
+      print(j)
+      j=j+2
