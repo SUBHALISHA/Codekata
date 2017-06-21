3ed89dd3447bb8bbfbb4f90b699fd339a4d9a9ee
SUBHASREE K <subhasree260497@gmail.com>
2017-06-21 13:50:32 +0530

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,7 +1,7 @@
-n=input("Enter number:")
-if(n>0):
-  print (str(n)+" is a positive number")
-elif(n<0):
-  print (str(n)+" is a negative number")
+num=input("Enter number:")
+if(num>0):
+  print (str(num)+" is a positive number")
+elif(num<0):
+  print (str(num)+" is a negative number")
 else:
   print ("The given number is 0")
