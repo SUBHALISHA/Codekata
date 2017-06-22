e8f3cc186c762388d38b00c4c48bbd3f89c97fe9
SUBHASREE K <subhasree260497@gmail.com>
2017-06-22 10:18:25 +0530

    Create odd_sum.py

---
+++ odd_sum.py
@@ -0,0 +1,9 @@
+sum=0
+osum=0
+for i in range(1,16):
+    sum=sum+i
+print("The sum of numbers from 1-15 is "+str(sum))
+while i < 46:
+    osum=osum+i
+    i=i+2
+print("The sum of odd numbers from 15-45 is "+str(osum))
