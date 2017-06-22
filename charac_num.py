3a74727d948173322fcada660a9ea35725cb3edf
SUBHASREE K <subhasree260497@gmail.com>
2017-06-22 10:32:19 +0530

    Update charac_num.py

--- charac_num.py
+++ charac_num.py
@@ -4,4 +4,4 @@ count=0
 for i in range(l):
     if st[i].isalpha():
         count+=1
-print "The  number of characters in "+st+" is "+str(count)
+print "The  number of characters in '"+st+"' is "+str(count)
