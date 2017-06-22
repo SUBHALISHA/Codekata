1cbf62f7fcbb6b972a15dd73eb4d9dee1bdc1b41
SUBHASREE K <subhasree260497@gmail.com>
2017-06-22 10:11:11 +0530

    Create exit_Q.py

---
+++ exit_Q.py
@@ -0,0 +1,7 @@
+while(True):
+    ch=input("Enter a character:")
+    if(ch=='Q'):
+        print("You have entered Q so terminating your program...")
+        break
+    else:
+        print("You have entered "+ch)
