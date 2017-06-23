4a5b1349dc4dd35ea8e34ac6b08b84cd4076c7b3
SUBHASREE K <subhasree260497@gmail.com>
2017-06-23 10:13:10 +0530

    Update word_num.py

--- word_num.py
+++ word_num.py
@@ -1,9 +1,5 @@
 st=input("Enter a line:")
-st=st.lstrip()
-st=st.rstrip()
-l=len(st)
-count=1
-for i in range(l):
-    if st[i].isspace():
-        count+=1
-print "The  number of words in '"+st+"' is "+str(count
+list=[]
+list=st.split()
+count=len(list)
+print "The  number of words in '"+st+"' is "+str(count)
