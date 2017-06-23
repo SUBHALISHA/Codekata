4dbe8f087f599746a1117196bd1c8d46d21bd6c4
SUBHASREE K <subhasree260497@gmail.com>
2017-06-23 10:21:49 +0530

    Create max_3.py

---
+++ max_3.py
@@ -0,0 +1,9 @@
+    a=input("Enter a:")
+    b=input("Enter b:")
+    c=input("Enter c:")
+    if(a>b and a>c):
+        print(str(a)+" is the largest number")
+    elif(b>c and b>a):
+        print(str(b)+" is the largest number")
+    else:
+        print(str(c)+" is the largest number")
