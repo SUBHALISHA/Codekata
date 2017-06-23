a722b44278417352c64219fcc769178b44fd033d
SUBHASREE K <subhasree260497@gmail.com>
2017-06-23 10:19:38 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,7 @@
+x=input("Enter first number x:")
+y=input("Enter second number y:")
+print ("Before swapping x="+str(x)+" y= "+str(y))
+t=x
+x=y
+y=t
+print ("After swapping x="+str(x)+" y= "+str(y))
