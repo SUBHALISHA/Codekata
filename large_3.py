2180ddaaaf31ad1c5ecd9922e9921ffa697e57b6
SUBHASREE K <subhasree260497@gmail.com>
2017-07-14 20:33:04 +0530

    Create large_3.py

---
+++ large_3.py
@@ -0,0 +1,9 @@
+    a1=input("Enter a1:")
+    b1=input("Enter b1:")
+    c1=input("Enter c1:")
+    if(a1>b1 and a1>c1):
+        print(str(a1)+" is the largest number")
+    elif(b1>c1 and b1>a1):
+        print(str(b1)+" is the largest number")
+    else:
+        print(str(c1)+" is the largest number")
